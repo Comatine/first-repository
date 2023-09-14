7959982f255e501642aa24f22ff2f7ed205fc26a
Comatine <Comatine00@gmail.com>
2023-09-14 14:16:10 +0900

    BRUHHHHHHHHHHHH

--- 실습용자료 - 복사본/max_profit.py
+++ 실습용자료 - 복사본/max_profit.py
@@ -4,6 +4,6 @@ def maxProfit_bruteforce (prices):
    for i, price in enumerate(prices):
        for j in range(i, len(prices)):
            max_price = max(prices[j] - price, max_price)
-
+	print("어 형이야")
    return
 
